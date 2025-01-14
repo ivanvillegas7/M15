21860efa07b2868b9c36373590c1d32da48fb935
Iván Villegas Pérez <89591182+ivanvillegas7@users.noreply.github.com>
2025-01-14 11:21:27 +0100

    Add files via upload

--- EINASTO/J-factor_plots.py
+++ EINASTO/J-factor_plots.py
@@ -36,6 +36,8 @@ def all_J():
     plt.xlabel(r'$\alpha_\text{int}$ [deg]')
     plt.ylabel(r'$J(\alpha_\text{int})$ [GeV$^2$ cm$^{-5}$]')
     plt.legend()
+    #plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
+    #plt.tight_layout()
     plt.grid(True)
     plt.xscale('log')
     plt.yscale('log')
@@ -62,23 +64,23 @@ def combined_data():
     plt.vlines(0.1, ymin, ymax, color='red', ls='dotted',\
                label=r'$\alpha=0.1^\text{o}$')
     plt.vlines(0.23, ymin, ymax, color='purple', ls='dashed',\
-               label=r'$\alpha(R_t=0.044\text{ kpc})=0.23^\text{o}$')
+               label=r'$\alpha(R_t=44\text{ pc})=0.23^\text{o}$')
     plt.vlines(0.3, ymin, ymax, color='pink', ls='dotted',\
-               label=r'$\alpha(R_t=0.058\text{ kpc})=0.3^\text{o}$')
+               label=r'$\alpha(R_t=58\text{ pc})=0.3^\text{o}$')
     plt.vlines(0.35, ymin, ymax, color='darkslategray', ls='dotted',\
-               label=r'$\alpha(R_t=0.067\text{ kpc})=0.35^\text{o}$')
+               label=r'$\alpha(R_t=67\text{ pc})=0.35^\text{o}$')
     plt.vlines(0.42, ymin, ymax, color='gold', ls='dotted',\
-               label=r'$\alpha(R_t=0.080\text{ kpc})=0.42^\text{o}$')
+               label=r'$\alpha(R_t=80\text{ pc})=0.42^\text{o}$')
     plt.vlines(0.5, ymin, ymax, color='green', ls='dashed',\
                label=r'$\alpha=0.5^\text{o}$')
     plt.xlabel(r'$\alpha_\text{int}$ [deg]')
     plt.ylabel(r'$J(\alpha_\text{int})$ [GeV$^2$ cm$^{-5}$]')
     plt.title(r"M15's $J$-factor")
-    plt.legend(loc='upper left')
+    plt.legend()
     plt.grid(True)
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlim(1e-2, 2)
+    plt.xlim(1e-3, 2)
     plt.ylim(ymin, ymax)
     plt.savefig('Results/J-factor_all_combined.pdf')
     
@@ -106,30 +108,30 @@ def combined_data():
                label=r'$\alpha=0.42^\text{o}$')
     plt.vlines(0.5, ymin, ymax, color='green', ls='dashed',\
                label=r'$\alpha=0.5^\text{o}$')
-    plt.plot(4.56e-5, 7.5e18, marker='X', label='H.E.S.S.', ls='none',\
+    plt.plot(0.07228242712521225, 7.5e18, marker='X', label='H.E.S.S.', ls='none',\
              color='black')
-    plt.plot(4.56e-5, 2.15e22, marker='X', ls='none', color='black')
-    plt.plot(4.56e-5, 7e19, marker='X', ls='none', color='black')
-    plt.vlines(0.0005, 2.457e16, 5.2648e17, label='Whipple', color='brown')
-    plt.hlines(2.457e16, 0.00055, 0.00045, color='brown')
-    plt.hlines(5.2648e17, 0.00055, 0.00045, color='brown')
-    plt.vlines(0.0005, 2.81e19, 7.02e19, color='brown')
-    plt.hlines(2.81e19, 0.00055, 0.00045, color='brown')
-    plt.hlines(7.02e19, 0.00055, 0.00045, color='brown')
+    plt.plot(0.07228242712521225, 2.15e22, marker='X', ls='none', color='black')
+    plt.plot(0.07228242712521225, 7e19, marker='X', ls='none', color='black')
+    plt.vlines(0.2503938552031453, 2.457e16, 5.2648e17, label='Whipple', color='brown')
+    plt.hlines(2.457e16, 0.3, 0.21, color='brown')
+    plt.hlines(5.2648e17, 0.3, 0.21, color='brown')
+    plt.vlines(0.2503938552031453, 2.81e19, 7.02e19, color='brown')
+    plt.hlines(2.81e19, 0.3, 0.21, color='brown')
+    plt.hlines(7.02e19, 0.3, 0.21, color='brown')
     plt.xlabel(r'$\alpha_\text{int}$ [deg]')
     plt.ylabel(r'$J(\alpha_\text{int})$ [GeV$^2$ cm$^{-5}$]')
     plt.title(r"M15's $J$-factor")
-    plt.legend(loc='lower center')
+    plt.legend()
     plt.grid(True)
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlim(4e-5, 5)
+    plt.xlim(1e-3, 5)
     plt.ylim(ymin, ymax)
     plt.savefig('Results/J-factor_all_combined_long.pdf')
     
 def main():
     
-    #all_J()
+    all_J()
     
     combined_data()
     
