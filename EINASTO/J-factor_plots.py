d1aab7a10b2f7284406438bda55d0413ba192bcd
Iván Villegas Pérez <89591182+ivanvillegas7@users.noreply.github.com>
2025-04-26 13:16:33 +0200

    Add files via upload

--- EINASTO/J-factor_plots.py
+++ EINASTO/J-factor_plots.py
@@ -2,6 +2,8 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import transform_abramowski
+import transform_wood
 
 def individual_J(R_t: str):
     
@@ -121,6 +123,11 @@ def combined_data():
     plt.ylim(ymin, ymax)
     plt.savefig('Results/J-factor_all_combined.pdf')
     
+    J_Wood=transform_wood.main()
+    
+    J_Abramowski=transform_abramowski.main()
+    
+    
     plt.figure()
     plt.plot(data[:, 0], data[:, 1], color='black', label='Median')
     plt.plot(data[:, 0], data[:, 5], color='blue')
@@ -132,7 +139,17 @@ def combined_data():
     plt.fill_between(data[:, 0], data[:, 7], data[:, 5], label=r'$95\%$ CL',\
                      color='wheat')
     plt.fill_between(data[:, 0], data[:, 6], data[:, 8], color='wheat')
-    (ymin, ymax) = (plt.ylim()[0], 1e27)
+    plt.plot(0.07228242712521225, J_Abramowski[0], marker='X', label='H.E.S.S.', ls='none',\
+             color='black')
+    plt.plot(0.07228242712521225, J_Abramowski[1], marker='X', ls='none', color='black')
+    plt.plot(0.07228242712521225, J_Abramowski[2], marker='X', ls='none', color='black')
+    plt.vlines(0.2503938552031453, J_Wood[0], J_Wood[1], label='Whipple', color='brown')
+    plt.hlines(J_Wood[0], 0.3, 0.21, color='brown')
+    plt.hlines(J_Wood[1], 0.3, 0.21, color='brown')
+    plt.vlines(0.2503938552031453, J_Wood[2], J_Wood[3], color='brown')
+    plt.hlines(J_Wood[2], 0.3, 0.21, color='brown')
+    plt.hlines(J_Wood[3], 0.3, 0.21, color='brown')
+    (ymin, ymax) = (1e18, 1e28)
     plt.vlines(0.1, ymin, ymax, color='red', ls='dashed',\
                label=r'$\alpha=0.1^\text{o}$')
     plt.vlines(0.23, ymin, ymax, color='purple', ls='dotted',\
@@ -145,16 +162,6 @@ def combined_data():
                label=r'$\alpha=0.42^\text{o}$')
     plt.vlines(0.5, ymin, ymax, color='green', ls='dashed',\
                label=r'$\alpha=0.5^\text{o}$')
-    plt.plot(0.07228242712521225, 7.5e18, marker='X', label='H.E.S.S.', ls='none',\
-             color='black')
-    plt.plot(0.07228242712521225, 2.15e22, marker='X', ls='none', color='black')
-    plt.plot(0.07228242712521225, 7e19, marker='X', ls='none', color='black')
-    plt.vlines(0.2503938552031453, 2.457e16, 5.2648e17, label='Whipple', color='brown')
-    plt.hlines(2.457e16, 0.3, 0.21, color='brown')
-    plt.hlines(5.2648e17, 0.3, 0.21, color='brown')
-    plt.vlines(0.2503938552031453, 2.81e19, 7.02e19, color='brown')
-    plt.hlines(2.81e19, 0.3, 0.21, color='brown')
-    plt.hlines(7.02e19, 0.3, 0.21, color='brown')
     plt.xlabel(r'$\alpha_\text{int}$ [deg]')
     plt.ylabel(r'$J(\alpha_\text{int})$ [GeV$^2$ cm$^{-5}$]')
     plt.title(r"M15's $J$-factor")
